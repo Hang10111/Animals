10db52bdb49526acca03b71c5ce42a8a2859a927
hang2310111 <hang2310111@gmail.com>
2019-09-17 22:49:27 +0700

    imp. Add Mapsui

---
+++ Animals.Client/Animals.Client/Converters/PositionToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Animals.Client.Converters
+{
+    public class PositionToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter != null && parameter is string)
+                return ((Xamarin.Forms.Maps.Position)value).ToString((string)parameter);
+
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Animals.Client/Animals.Client/Views/DetailPage.xaml.cs
+++ Animals.Client/Animals.Client/Views/DetailPage.xaml.cs
@@ -1,18 +1,19 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Mapsui.Layers;
+using BruTile.Predefined;
+using Mapsui.Widgets.ScaleBar;
 
 namespace Animals.Client.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailPage : ContentPage
     {
-
-
         public DetailPage()
         {
-
             InitializeComponent();
+            mapView.Map.Layers.Add(new TileLayer(KnownTileSources.Create(KnownTileSource.OpenStreetMap)) { Name = "OpenStreetMap" });
+            mapView.Map.Widgets.Add(new ScaleBarWidget(mapView.Map));
         }
     }
-           
 }
