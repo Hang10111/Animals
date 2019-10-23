d7fe3eed3b3a8b89cf97c8b5fadbc12ba4b2e0ee
hang2310111 <hang2310111@gmail.com>
2019-10-23 22:17:04 +0700

    mod. fix ScannerPage

--- Animals.Client/Animals.Client/ViewModels/ScannerPageViewModel.cs
+++ Animals.Client/Animals.Client/ViewModels/ScannerPageViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using Prism.Commands;
 using Prism.Navigation;
+using Prism.Services;
 
 namespace Animals.Client.ViewModels
 {
@@ -16,32 +18,46 @@ namespace Animals.Client.ViewModels
             get => _isTurnOnFlash;
             set => SetProperty(ref _isTurnOnFlash, value);
         }
+        bool _isProcessing = false;
+
+        private bool _isAnalyzing = true;
         public bool IsAnalyzing
         {
             get => _isAnalyzing;
             set => SetProperty(ref _isAnalyzing, value);
         }
+
+        private bool _isScanning;
         public bool IsScanning
         {
             get => _isScanning;
             set => SetProperty(ref _isScanning, value);
         }
+
+        private ZXing.Result _result;
         public ZXing.Result Result
         {
             get => _result;
             set => SetProperty(ref _result, value);
         }
 
-        private bool _isAnalyzing = true;
-        private bool _isScanning = true;
-        private ZXing.Result _result;
+
+        //private readonly INavigationService _navigationService;
+        //private readonly IPageDialogService _pageDialogue;
+        private readonly IDeviceService _deviceService;
+
+
         private bool _isTurnOnFlash;
 
-        public ScannerPageViewModel(INavigationService navigationService) : base(navigationService)
+        public ScannerPageViewModel(INavigationService navigationService, IDeviceService deviceService) : base(navigationService)
         {
+            //_navigationService = navigationService;
+            //_pageDialogue = pageDialogService;
+            _deviceService = deviceService;
             Title = "Scan";
             TurnOnFlashCommand = new DelegateCommand(HandleTurnOnFlashCommand);
             OnBarcodeScannedCommand = new DelegateCommand(OnBarcodeScanned, canExecute);
+            _isScanning = true;
         }
 
         public override void OnNavigatedTo(INavigationParameters parameters)
@@ -71,14 +87,25 @@ namespace Animals.Client.ViewModels
 
         private async void OnBarcodeScanned()
         {
-            IsBusy = true;
-            IsAnalyzing = false;
-            var animalItems = await Services.AnimalService.GetAnimals();
-            var animal = animalItems.FirstOrDefault(item => item.IdSinhVat.ToString() == Result.Text);
-            var parameters = new NavigationParameters();
-            parameters.Add("sinhvat", animal);
-            await NavigationService.NavigateAsync("Detail", parameters);
-            IsBusy = false;
+            if (!_isProcessing)
+            {
+                _deviceService.BeginInvokeOnMainThread(async () =>
+                {
+                    _isProcessing = true;
+                    _isAnalyzing = false;
+                    RaisePropertyChanged("IsAnalyzing");
+                    string message = Result.Text;
+                    Debug.WriteLine(message);
+                    var animalItems = await Services.AnimalService.GetAnimals();
+                    var animal = animalItems.FirstOrDefault(item => item.IdSinhVat.ToString() == message);
+                    var parameters = new NavigationParameters();
+                    parameters.Add("sinhvat", animal);
+                    await NavigationService.NavigateAsync("Detail", parameters); 
+                    _isProcessing = false;
+                    _isAnalyzing = true;
+                    RaisePropertyChanged("IsAnalyzing");
+                });
+            }
         }
     }
 }
